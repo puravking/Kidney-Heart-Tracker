440abfe1f154a3f599d1261ea369a710e491d3a8
Purav <120137918+puravking@users.noreply.github.com>
2024-10-05 21:00:38 +0530

    Create middlewaresAuthentication.js

---
+++ middlewaresAuthentication.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/authentication",(req,res)=>{
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const kidneyId = req.query.kidneys;
+
+    if((username != "purav" ||
+         password != "purav1234")) {
+        res.status(400).json({
+            message:"Wrong id or password!!"
+        })
+        return
+    }
+    if(kidneyId != 1 && kidneyId != 2){
+            res.status(400).json({
+                message:"Wrong input"
+            })  
+            return      
+    }
+    res.json({
+        message:"Your kidney is fine"
+    })
+});
+app.listen(3000);
